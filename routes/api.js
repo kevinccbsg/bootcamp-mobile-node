c4bde6f660daf3a0e6a1963606a2bb042e1ff8a1
kevin Martínez <kevinccbsg@gmail.com>
2018-12-17 20:59:34 +0100

    Add new endpoints for user

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getAnuncio } = require('../controllers/anuncio');
-const { register } = require('../controllers/user');
+const { register, authenticate } = require('../controllers/user');
 
 const router = express.Router();
 
 router.get('/v1/anuncios', getAnuncio);
 router.post('/v1/user', register);
+router.post('/v1/user/login', authenticate);
 
 module.exports = router;
